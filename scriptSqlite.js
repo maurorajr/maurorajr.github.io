a8e3611e4215e37d99961730b17bf7dad7be0bfd
Mauro Reis <maurorajr@gmail.com>
2023-02-09 17:58:27 -0300

    Add files via upload

--- scriptSqlite.js
+++ scriptSqlite.js
@@ -3,7 +3,7 @@ function con() {
     this.bdSql = window.openDatabase('csGOEMPIRE', '1.0', 'cosgoempire', 2 * 1024 * 1024);
 }
 
-con.prototype. = function () {
+con.prototype.openTableTopPlayers = function () {
     this.bdSql.transaction(function (tx) {
         tx.executeSql('CREATE TABLE TOPPLAYERS(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, totalBet FLOAT)', [], function (tx, msg) {
             console.log('Created')
