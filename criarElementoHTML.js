e2977262f44e63de760c95014137d02a6c51d5f4
Mauro Reis <maurorajr@gmail.com>
2023-05-26 12:27:41 -0300

    Update criarElementoHTML.js

--- criarElementoHTML.js
+++ criarElementoHTML.js
@@ -1,13 +1,3 @@
-// Crie um elemento <script>
 var script = document.createElement('script');
-
-// Defina o código JavaScript que você deseja adicionar
-var code = `
-  // Seu código JavaScript aqui
-`;
-
-// Atribua o código ao elemento <script>
-script.textContent = code;
-
-// Adicione o elemento <script> ao final do elemento <body> ou <head> do documento
-document.body.appendChild(script);
+script.src = 'https://raw.githubusercontent.com/seu-usuario/seu-repositorio/seu-branch/seu-arquivo.js';
+document.head.appendChild(script);
