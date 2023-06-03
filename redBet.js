a3b33a4c7f30f1292f1f1d6753f988883ac1245d
Mauro Reis <maurorajr@gmail.com>
2023-06-03 16:24:27 -0300

    Update redBet.js
    Refatorado

--- redBet.js
+++ redBet.js
@@ -1,33 +1,32 @@
-var mult = 49000, amount, betAmounts, reds, history;
-// Função para formatar o fundo
-function formatBackground() {
-    // Seleciona todos os elementos com a classe "bet-amount"
-    betAmounts = document.getElementsByClassName('bet-amount');
-    history = [['Página', 'Multiplicador']];
-    
-    // Itera sobre os elementos
-    reds = 0;
-    for (var i = 0; i < betAmounts.length; i++) {
-        amount = betAmounts[i].textContent.trim(); // Obtém o valor do elemento e remove espaços em branco
-        amount = parseInt(amount.replace(/\./g, '').split(',')[0], 10); // Converte a string em um número
+let mult = 49000;
+let betAmounts, history;
 
-        // Verifica se o número é maior que 99
-        if (amount >= mult) {
-            betAmounts[i].style.backgroundColor = 'red'; // Define o fundo como vermelho
-            reds++            
-        }
+function formatBackground() {
+  betAmounts = document.getElementsByClassName('bet-amount');
+  history = [['Página', 'Multiplicador']];
+
+  let reds = 0;
+  Array.from(betAmounts).forEach((element) => {
+    const page = document.getElementsByClassName('pagination-custom')[0].textContent.replace('Página ', '');
+    let amount = parseInt(element.textContent.trim().replace(/\./g, '').split(',')[0], 10);
+
+    if (amount >= mult) {
+      element.style.backgroundColor = 'red';
+      history.push([page, amount]);
+      reds++;
     }
-    if (reds == 0) {
-        clickNextPage();
-    } else {
-        console.log('Qtd' + reds.toString() + ' | ');
-    }
+  });
+
+  if (reds === 0) {
+    clickNextPage();
+  } else {
+    console.table(history);
+  }
 }
 
-// Executa a formatação a cada segundo
 setInterval(formatBackground, 1000);
 
-function clickNextPage() {    
-    const next = document.getElementsByClassName('pagination__button')[1];
-    next.click();
+function clickNextPage() {
+  const nextButton = document.getElementsByClassName('pagination__button')[1];
+  nextButton.click();
 }
