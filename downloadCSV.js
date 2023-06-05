a9fb756bf61977cc8b49a853ca7b7797f6c09003
Mauro Reis <maurorajr@gmail.com>
2023-06-05 18:00:40 -0300

    Create downloadCSV.js

---
+++ downloadCSV.js
@@ -0,0 +1,31 @@
+function downloadCSV(data, filename) {
+    const csv = arrayToCSV(data);
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    
+    if (link.download !== undefined) {
+      const url = URL.createObjectURL(blob);
+      link.setAttribute('href', url);
+      link.setAttribute('download', filename);
+      link.style.visibility = 'hidden';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  }
+  
+  function arrayToCSV(data) {
+    const csvRows = [];
+    const headers = Object.keys(data[0]);
+    csvRows.push(headers.join(','));
+  
+    for (const row of data) {
+      const values = headers.map(header => {
+        const escaped = ('' + row[header]).replace(/"/g, '\\"');
+        return `"${escaped}"`;
+      });
+      csvRows.push(values.join(','));
+    }
+  
+    return csvRows.join('\n');
+  }
