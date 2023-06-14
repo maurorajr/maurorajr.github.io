4a86aaf1ebd5683ed939f8c239d933b813fac948
Mauro Reis <maurorajr@gmail.com>
2023-06-13 21:31:59 -0300

    Create redHistoryV1.js

---
+++ redHistoryV1.js
@@ -0,0 +1,20 @@
+var tempo = 2000; // Tempo em milissegundos (2 segundos)
+var valorLimite = 89; // Valor limite para colorir
+
+function colorirValoresAcimaDeLimite() {
+  var betAmounts = document.getElementsByClassName('bet-amount');
+
+  for (var i = 0; i < betAmounts.length; i++) {
+    var betAmount = parseFloat(betAmounts[i].textContent.replace(',', '.'));
+
+    if (betAmount > valorLimite) {
+      betAmounts[i].style.backgroundColor = 'red';
+    }
+  }
+}
+
+// Executar a função inicialmente
+colorirValoresAcimaDeLimite();
+
+// Executar a função a cada intervalo de tempo definido
+setInterval(colorirValoresAcimaDeLimite, tempo);
